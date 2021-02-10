ff5d145b2af696e4862a2eeed567cac555fd2b38
halilgithub <halilkariyermail@gmail.com>
2021-02-10 14:23:54 +0300

    test for different key types

--- HashMap_Test.cpp
+++ HashMap_Test.cpp
@@ -8,7 +8,7 @@ int main(){
     HashMap<std::string,int> hmap;
 
     if (hmap.isEmpty())
-        std::cout << "Hasmap is empty !" << std::endl;
+        std::cout << "Hashmap is empty !" << std::endl;
     else
         std::cout << "Hashmap is not empty" << std::endl;
 
@@ -44,6 +44,40 @@ int main(){
     std::cout << std::endl;
 
     hmap.printHashMap();
+
+    HashMap<int,int> hmap2;
+
+     if (hmap2.isEmpty())
+        std::cout << "Hashmap is empty !" << std::endl;
+    else
+        std::cout << "Hashmap is not empty" << std::endl;
+
+    std::cout << std::endl;
+
+    bool success2 = hmap2.insert(12, 1201);
+    success2 = hmap2.insert(18, 1805);
+    success2 = hmap2.insert(13, 1307);
+    success2 = hmap2.insert(14, 1402);
+    success2 = hmap2.insert(22, 2209);
+
+
+    auto key2 = 13;
+    auto value2 = hmap2.search(key2);
+    if(value2.has_value())
+        std::cout << "[INFO] The element exists: (" << key2 << ", " << value2.value() << ")" << std::endl;
+    else
+        std::cout << "[INFO] An element with key:" << key2 << " doesn't exist !" << std::endl;
+
+    key2 = 30;
+    value2 = hmap2.search(key2);
+    if(value2.has_value())
+        std::cout << "[INFO] The element exists: (" << key2 << ", " << value2.value() << ")" << std::endl;
+    else
+        std::cout << "[INFO] An element with key:" << key2 << " doesn't exist !" << std::endl;
+
+    std::cout << std::endl;
+
+    hmap2.printHashMap();
 
     return 0;
 }
